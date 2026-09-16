3f1d42bb38d2cc434376da5f9de52145ab6abbef
BenoitCote <benoitcote@anlvpn210.mcs.anl.gov>
2026-09-16 13:48:10 -0700

    added failure if .env not populated

--- 01_Facility_API/config.py
+++ 01_Facility_API/config.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 
 from alcf_tokens.auth import get_access_token, ServiceName
@@ -9,12 +10,15 @@ HEADERS = {
 }
 
 load_dotenv()
-RESOURCE_ID = os.environ.get("RESOURCE_ID")
-NODES = os.environ.get("NODES")
-WALLTIME_SEC = os.environ.get("WALLTIME_SEC")
-QUEUE = os.environ.get("QUEUE")
-COMPUTE_ALLOCATION = os.environ.get("COMPUTE_ALLOCATION")
-STDOUT_PATH = os.environ.get("STDOUT_PATH")
-STDERR_PATH = os.environ.get("STDERR_PATH")
-COMMANDS = os.environ.get("COMMANDS")
-
+try:
+    RESOURCE_ID = os.environ["RESOURCE_ID"]
+    NODES = os.environ.get("NODES")
+    WALLTIME_SEC = os.environ.get("WALLTIME_SEC")
+    QUEUE = os.environ.get("QUEUE")
+    COMPUTE_ALLOCATION = os.environ.get("COMPUTE_ALLOCATION")
+    STDOUT_PATH = os.environ.get("STDOUT_PATH")
+    STDERR_PATH = os.environ.get("STDERR_PATH")
+    COMMANDS = os.environ.get("COMMANDS")
+except KeyError as e:
+    print(f"Missing environment variable: {e}")
+    sys.exit(1)
