e266a41cc5549bcaf81bbb4fcc5a8154d62353a8
BenoitCote <benoitcote@BenoitCotes-MacBook-Pro.local>
2026-09-22 13:57:52 -0700

    removed extra space

--- 01_Facility_API/03_submit_job.py
+++ 01_Facility_API/03_submit_job.py
@@ -8,7 +8,6 @@ import requests
 
 from utils import HEADERS
 
-
 # Select compute cluster
 #RESOURCE_ID="8b9b42f7-572a-4909-8472-a0453436304c" # Crux
 RESOURCE_ID="55c1c993-1124-47f9-b823-514ba3849a9a" # Polaris
